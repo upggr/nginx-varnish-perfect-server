2a6bb2dfbdb47e6c524d6fe29fdadabc6a545072
Ioannis Kokkinis <info@upg.gr>
2016-03-22 00:22:12 +0000

    d

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Welcome to the installation.."
-echo "Creating a new user "
+echo "Creating a new user.. "
 read -p 'Username: ' uservar
 echo "You entered: $uservar - setting up user..."
 
