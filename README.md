e790ce7d8850aacf2557d08b0b56de9203584e00
Ioannis Kokkinis <info@upg.gr>
2016-03-22 23:56:00 +0000

    done

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # nginx-varnish-perfect-server
 
 Usage :
-Head to [digital ocean](https://m.do.co/c/6e83df0e17c6) or wherever, login as root, and run the following :
+Head to [digital ocean](https://m.do.co/c/6e83df0e17c6) or wherever, create a 5-10$ server, login as root, and run the following :
 `sudo apt-get install curl -y
 curl -sL https://raw.githubusercontent.com/upggr/nginx-varnish-perfect-server/master/install.sh | sudo bash -`
 
@@ -10,3 +10,4 @@ When the server reboots, ssh on port 9022 as root and run the following to creat
 
 Then, just visit domain.com (provided you have pointed dns to your ip) and point to your mysql server. Run the same for as may domains as needed.
 
+Here you have it, 1.000.000 hits/per day capable wordpress instance for 10$
--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+wget https://raw.githubusercontent.com/upggr/nginx-varnish-perfect-server/master/default
+wget https://raw.githubusercontent.com/upggr/nginx-varnish-perfect-server/master/default.vcl
+wget https://raw.githubusercontent.com/upggr/nginx-varnish-perfect-server/master/newsite.sh
+wget https://raw.githubusercontent.com/upggr/nginx-varnish-perfect-server/master/nginx-installer.sh
+wget https://raw.githubusercontent.com/upggr/nginx-varnish-perfect-server/master/nginx.conf
+wget https://raw.githubusercontent.com/upggr/nginx-varnish-perfect-server/master/varnish
+wget https://raw.githubusercontent.com/upggr/nginx-varnish-perfect-server/master/vsftpd.conf
+wget https://raw.githubusercontent.com/upggr/nginx-varnish-perfect-server/master/wordpress.template
+chmod +x newsite.sh
+chmod +x nginx-installer.sh
