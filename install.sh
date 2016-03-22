20a4e2a12506f1185b6da9d3ddf8023154eaa84b
Ioannis Kokkinis <info@upg.gr>
2016-03-22 00:12:00 +0000

    a

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 echo "Welcome to the installation.."
 echo "First of all, to secure your installation, you will be creating a user so you're not using root!"
-read -p 'Username: ' uservar
-echo "You entered: $uservar - setting up user..."
-adduser $uservar
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -s -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p $pass $username
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system"
+	exit 2
+fi
