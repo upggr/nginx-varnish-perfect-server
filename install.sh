a49fd461a48030c7b0d5999d6907bfa09057ca04
Ioannis Kokkinis <info@upg.gr>
2016-03-22 00:09:08 +0000

    a

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 echo "Welcome to the installation.."
 echo "First of all, to secure your installation, you will be creating a user so you're not using root!"
-echo "Enter username - and then follow the prompts to create password etc"
 read -p 'Username: ' uservar
 echo "You entered: $uservar - setting up user..."
 adduser $uservar
