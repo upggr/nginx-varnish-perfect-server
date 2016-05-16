3f28b1974c1355aae2233cf21cd53079dbb17b7b
Ioannis Kokkinis <info@upg.gr>
2016-05-16 19:27:57 +0300

    Updated fresh-install.shvsftpd.conf .

--- fresh-install.sh
+++ fresh-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Welcome to the installation.."
 sudo apt-get update -y
-sudo apt-get install htop nginx php7.0-cli php7.0-curl php7.0-dev php7.0-fpm php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-opcache php-mbstring php7.0-mbstring php7.0-sybase freetds-common libsybdb5 php-gettext mariadb-server mariadb-client unzip varnish ufw -y
+sudo apt-get install htop nginx php7.0-cli php7.0-curl php7.0-dev php7.0-fpm php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-opcache php-mbstring php7.0-mbstring php7.0-sybase vsftpd freetds-common libsybdb5 php-gettext mariadb-server mariadb-client unzip varnish ufw -y
 sudo service php7.0-fpm restart
 sudo service nginx restart
 sudo service varnish restart
@@ -21,11 +21,15 @@ sudo rm /etc/default/varnish
 sudo cp varnish /etc/default/varnish
 sudo rm /etc/varnish/default.vcl
 sudo cp default.vcl /etc/varnish/default.vcl
+/etc/vsftpd.conf
+sudo rm /etc/vsftpd.conf
+sudo cp vsftpd.conf /etc/vsftpd.conf
 sudo rm /lib/systemd/system/varnish.service
 sudo cp varnish.service /lib/systemd/system/varnish.service
 sudo cp /lib/systemd/system/varnish.service /etc/systemd/system/
 sudo systemctl daemon-reload
 #sudo sed -i 's/6081/80/g' /etc/systemd/system/varnish.service
+touch /etc/vsftpd.allowed_users
 sudo service varnish restart
 sudo rm /var/www/html/index.nginx-debian.html
 sudo cp index.php /var/www/html/index.php
